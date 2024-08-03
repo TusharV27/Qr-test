bfc45b2a9cda41d7c243e133fe9997cc319fe90f
Tushar <tusharvaghasiya369@gmail.com>
2024-08-03 08:21:23 +0530

    4

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Page from './Page'
+import StartPage from './StartPage'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Page />}></Route>
+          <Route path='/' element={<StartPage />}></Route>
           <Route path='/:restName/:restId' element={<Page />}></Route>
         </Routes>
       </BrowserRouter>
